8812a058214ea4abf065df46ea1407672721b72c
Felix Kwemoi <felixkwemoi7@gmail.com>
2025-06-20 12:52:24 +0300

    Fix: use wget to reliably download config.yaml

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Download config.yaml from Google Drive
-curl -L -o config.yaml "https://drive.google.com/uc?export=download&id=1sRoqZrTNgLDJIbgEuvcNzIJZEjdL4Wei"
+# Function to download Google Drive file using file ID
+FILE_ID="1sRoqZrTNgLDJIbgEuvcNzIJZEjdL4Wei"
+DESTINATION="config.yaml"
+
+echo "Downloading config.yaml from Google Drive..."
+
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=${FILE_ID}" -O ${DESTINATION}
 
 # Install dependencies
 pip install -r requirements.txt
